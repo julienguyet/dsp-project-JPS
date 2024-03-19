adcdd99a6860966c76a545c8983aced87d4bd934
Julien Guyet <jules.guyet@gmail.com>
2024-03-19 18:15:35 +0100

    updating make_and_save_prediction function to handle dictionary

--- code/API/predictions.py
+++ code/API/predictions.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import pandas as pd
-from sales_prediction.inference import *
 import sys
-sys.path.append('../../house_prices/')
+import os
+sys.path.append(os.path.abspath('../..'))
+from sales_prediction.inference import *
+
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Create SQLAlchemy engine
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Lufiva@localhost:5432/predictions"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Lufiva@localhost:5432/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Create SQLAlchemy session factory
@@ -21,8 +23,17 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Define SQLAlchemy Base
 Base = declarative_base()
 
+# Define Prediction model
+class Prediction(Base):
+    __tablename__ = "prediction"
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    prediction_value = Column(Float)
+    features_used = Column(String)
+
 # Function to make predictions and save to database
-def make_predictions_and_save(input_data: pd.DataFrame):
+def make_predictions_and_save(input_data: dict):
 
     predictions = make_predictions(input_data)
 
@@ -33,16 +44,25 @@ def make_predictions_and_save(input_data: pd.DataFrame):
 
     # Save to database
     with SessionLocal() as db:
-        prediction_record = Prediction(timestamp=datetime.utcnow(), prediction_value=prediction_value, features_used=features_used)
+        prediction_record = Prediction(prediction_value=prediction_value, features_used=str(features_used))
         db.add(prediction_record)
         db.commit()
         db.refresh(prediction_record)
 
     return predictions
 
-# API endpoint to make predictions and save to database
+'''# API endpoint to make predictions and save to database
 @app.post("/predict/")
 async def predict(input_data: dict):
     df = pd.DataFrame(input_data)
     predictions = make_predictions_and_save(df)
+    return predictions '''
+
+# API endpoint to make predictions and save to database
+# API endpoint to make predictions and save to database
+@app.post("/predict/")
+async def predict(input_data: dict):
+    print("Received request body:", input_data)  # Print the received request body
+    predictions = make_predictions_and_save(input_data)
     return predictions
+
