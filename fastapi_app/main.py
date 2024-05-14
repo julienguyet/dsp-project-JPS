41df8014a5360d1595a064e7e4b5ff014776f912
preetha-pallavi <preethamishra76@gmail.com>
2024-05-14 13:58:42 +0200

    generating files for test purposes

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -25,7 +25,7 @@ from fastapi import File , UploadFile
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://siva:siva@localhost/jsp"
+DATABASE_URL = "postgresql://preetha:preetha@localhost/jsp"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
